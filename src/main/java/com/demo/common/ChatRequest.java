c4e9845ae4736bab4121c8ed95634a2208cb56f2
Zhao <d726760649@gmail.com>
2026-04-17 01:52:41 +0800

    feat: add args in ChatRequest

--- src/main/java/com/demo/common/ChatRequest.java
+++ src/main/java/com/demo/common/ChatRequest.java
@@ -4,6 +4,19 @@ import lombok.Data;
 
 @Data
 public class ChatRequest {
+
     private String message;
-    private String userId; // 为后面多轮对话准备
+
+    private String userId;
+
+    private Boolean stream = true;
+
+    private Boolean enableSearch = true;
+
+    private Double temperature = 0.75;
+
+    private Integer maxTokens = 1024;
+
+    private Double topP = 0.9;
+
 }
