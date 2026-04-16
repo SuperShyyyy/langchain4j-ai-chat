7dfd0b74fb9295725cb3fd755b774ee4513221bc
Zhao <d726760649@gmail.com>
2026-04-17 02:00:43 +0800

    feat: add the logging function

--- src/main/java/com/demo/config/APIConfig.java
+++ src/main/java/com/demo/config/APIConfig.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class APIConfig {
-    @Value("${demo.api.key}")
+    @Value("${langchain4j.open-ai.chat-model.api-key}")
     String apiKey;
-    @Value("${demo.api.base-url}")
+    @Value("${langchain4j.open-ai.chat-model.base-url}")
     String baseUrl;
-    @Value("${demo.api.model-name}")
+    @Value("${langchain4j.open-ai.chat-model.model-name}")
     String modelName;
     public String getBaseUrl() {
         return baseUrl;
--- src/main/java/com/demo/service/impl/ChatServiceImpl.java
+++ src/main/java/com/demo/service/impl/ChatServiceImpl.java
@@ -1,6 +1,5 @@
 package com.demo.service.impl;
 
-import com.demo.config.APIConfig;
 import com.demo.service.ChatService;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
